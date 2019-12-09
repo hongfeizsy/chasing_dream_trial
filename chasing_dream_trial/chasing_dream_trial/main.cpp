86dd8e829bfb570bef93bfb37c82c4ad4277e51d
hongfeizsy <hongfeizsy@gmail.com>
2019-12-09 22:52:13 +0100

    Hello world.

---
+++ chasing_dream_trial/chasing_dream_trial/main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+#include <boost/timer.hpp>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	
+	MessageBox(NULL, L"Hello Visual Studio! \n\n I'm Harry.", L"Message box", 0);
+	return 0;
+}
+
+
